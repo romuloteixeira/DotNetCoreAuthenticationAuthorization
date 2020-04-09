6944789188e6f254767685e7e5700960fba6c9dd
romulo_teixeira <romulo.teixeira@hotmail.com>
2020-04-09 17:04:34 +0100

    Starting the project

---
+++ Basics/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
